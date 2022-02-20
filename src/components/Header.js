8e799b6925f7821e2082a027f521d75c0acd79e6
sockeye119 <58209598+sockeye119@users.noreply.github.com>
2022-02-20 12:39:19 +0900

    Initial Commit

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className='header'>
+        
+        <div className='header-logo'>
+          <img src='https://assets.st-note.com/production/uploads/images/37814703/profile_110db444cdbfd1f62c65bc4f732f4aaa.png?width=200&crop=1:1,smart' />
+      
+        </div>
+        
+
+        
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import Lesson from './Lesson';
+
+class Main extends React.Component {
+  render() {
+    const lessonList = [
+      {
+        
+        image: 'https://plaid.co.jp/assets/img/press/media/p140825/plaid-logo.png',
+        introduction: '説明',
+      },
+      {
+        
+        image: 'https://plaid.co.jp/assets/img/press/media/p140825/plaid-logo.png',
+        introduction: '説明',
+      }, {
+        
+        image: 'https://plaid.co.jp/assets/img/press/media/p140825/plaid-logo.png',
+        introduction: '説明',
+      }, {
+        
+        image: 'https://plaid.co.jp/assets/img/press/media/p140825/plaid-logo.png',
+        introduction: '説明',
+      }, {
+        
+        image: 'https://plaid.co.jp/assets/img/press/media/p140825/plaid-logo.png',
+        introduction: '説明',
+      }, {
+        
+        image: 'https://plaid.co.jp/assets/img/press/media/p140825/plaid-logo.png',
+        introduction: '説明',
+      }, {
+        
+        image: 'https://plaid.co.jp/assets/img/press/media/p140825/plaid-logo.png',
+        introduction: '説明',
+      },
+      
+    ];
+
+    return (
+      <div className='main-wrapper'>
+        <div className='main'>
+          <div className='copy-container'>
+            <h1>IR museum</h1>
+            
+          </div>
+           
+          <div className='lesson-container'>
+            <h3>最新</h3>
+            {lessonList.map((lessonItem) => {
+              return (
+                <Lesson
+                  name={lessonItem.name}
+                  image={lessonItem.image}
+                  introduction={lessonItem.introduction}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Main;
